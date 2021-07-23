f9a550506c068fd00bb35be07e2657c95755ad0f
Ruslana17 <55513221+Ruslana17@users.noreply.github.com>
2021-07-23 12:35:25 +0200

    Update random.js

--- random.js
+++ random.js
@@ -1,12 +1,12 @@
 let hello='Hello, what you name?';
-let n=promt(hello);
+let n=prompt(hello);
 
 while(n=='' || n==null){
-    n=promt(hello);
+    n=prompt(hello);
 }
 alert (n + ', I thought of a number from 1 to 100. Try to guess it in the fewest possible attempts. After each attempt I will say "little", "a lot", or "guessed right".');
 
-let number=rendom(100);
+let number=random(100);
 let guess=prompt('What number did I think?');
 let numberOfGuesses=0;
 
